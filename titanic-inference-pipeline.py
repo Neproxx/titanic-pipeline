395f9303a9254b40b5ca5095a8a3a07d923308a4
Axel Kaliff <axel.kaliff@protonmail.com>
2022-11-17 23:37:20 +0100

    inference added for testing

---
+++ titanic-inference-pipeline.py
@@ -0,0 +1,114 @@
+import os
+import modal
+    
+LOCAL=False
+
+os.environ["HOPSWORKS_API_KEY"] = "Y6l4kEnIzbD1AutQ.RCUi4xT4wliiAO1XFwZr2YmO1DfbZQE25DM9ybYz7yz2kWyrf6hokVLyLN4SiDWy"
+
+if LOCAL == False:
+   stub = modal.Stub()
+   hopsworks_image = modal.Image.debian_slim().pip_install(["hopsworks","joblib","seaborn","scikit-learn","dataframe-image"])
+   @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("hopsworks"))
+   def f():
+       g()
+
+def g():
+    import pandas as pd
+    import hopsworks
+    import joblib
+    import datetime
+    from PIL import Image
+    from datetime import datetime
+    import dataframe_image as dfi
+    from sklearn.metrics import confusion_matrix
+    from matplotlib import pyplot
+    import seaborn as sns
+    import requests
+
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+    
+    mr = project.get_model_registry()
+    model = mr.get_model("titanic_modal", version=1)
+    model_dir = model.download()
+    model = joblib.load(model_dir + "/titanic_model.pkl")
+    
+    # Get the latest inserted batch data element
+    feature_view = fs.get_feature_view(name="titanic_modal", version=1)
+    batch_data = feature_view.get_batch_data()
+    
+    y_pred = model.predict(batch_data)
+    # print(y_pred)
+    passenger = y_pred[y_pred.size-1]
+    dataset_api = project.get_dataset_api()
+
+    print(f'Survival predicted: {passenger}')
+
+    ## PREDICTION COMPLETE
+
+    titanic_fg = fs.get_feature_group(name="titanic_modal", version=1)
+    df = titanic_fg.read()
+
+    # get the label from the last added element
+    label = df.iloc[-1]["survived"]
+    if label == 0:
+        label_name = "Dead"
+    else:
+        label_name = "Survived"
+
+    print("Actual Survivial status: " + label_name)
+
+    monitor_fg = fs.get_or_create_feature_group(name="titanic-predictions",
+                                                version=1,
+                                                primary_key=["datetime"],
+                                                description="Titanic Passenger Survival Prediction/Outcome Monitoring"
+                                                )
+    
+    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    data = {
+        'prediction': [passenger],
+        'label': [label],
+        'datetime': [now],
+       }
+
+    # add prediction with relevant metadata to monitor feature group
+    monitor_df = pd.DataFrame(data)
+    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
+    
+    history_df = monitor_fg.read()
+    # Add our prediction to the history, as the history_df won't have it - 
+    # the insertion was done asynchronously, so it will take ~1 min to land on App
+    history_df = pd.concat([history_df, monitor_df])
+
+    # Upload picture of recents for some reason
+    df_recent = history_df.tail(5)
+    dfi.export(df_recent, './df_recent.png', table_conversion = 'matplotlib')
+    dataset_api.upload("./df_recent.png", "Resources/images", overwrite=True)
+    
+    predictions = history_df[['prediction']]
+    labels = history_df[['label']]
+
+    # Only create the confusion matrix when our iris_predictions feature group has examples of all 3 iris flowers
+    print("Number of predictions to date: " + str(predictions.value_counts().count()))
+    if predictions.value_counts().count() == 3:
+        results = confusion_matrix(labels, predictions)
+    
+        df_cm = pd.DataFrame(results, ['True Dead', 'True Survivor'],
+                             ['Pred Dead', 'Pred Survivor'])
+    
+        cm = sns.heatmap(df_cm, annot=True)
+        fig = cm.get_figure()
+        fig.savefig("./confusion_matrix.png")
+        dataset_api.upload("./confusion_matrix.png", "Resources/images", overwrite=True)
+    else:
+        print("You need 3 different passenger predictions to create the confusion matrix.")
+        print("Run the batch inference pipeline more times until you get 3 different passenger predictions") 
+
+
+if __name__ == "__main__":
+    if LOCAL == True :
+        g()
+    else:
+        with stub.run():
+            f()
+
